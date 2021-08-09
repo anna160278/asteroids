d408da80ba7b1be693ee74894433175608d242f3
John Titor <J0l-lN_TiT0R@pm.me>
2021-08-09 11:22:02 +0300

    add asteroids and background

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import os
+import random as rnd
+
 import pygame as p
+
 import sprites
+from settings import *
 
-
-SCREEN_WIDTH = 900
-SCREEN_HEIGHT = 600
 
 # Loading images
 ship_img = p.image.load('res/PNG/playerShip1_orange.png')
+background_img = p.image.load('res/Backgrounds/darkPurple.png')
+background_img = p.transform.scale(background_img,
+                                   (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+meteor_name_list = os.listdir('res/PNG/Meteors')
 
 # Initializing the game window
 p.init()
@@ -16,6 +23,10 @@ p.display.set_caption('Asteroids')
 
 ship = sprites.Spaceship((SCREEN_WIDTH/2, SCREEN_HEIGHT-50),
                          ship_img)
+meteor_group = p.sprite.Group()
+
+SPAWN_METEOR = p.USEREVENT
+p.time.set_timer(SPAWN_METEOR, 300)
 
 running = True
 while running:
@@ -24,8 +35,23 @@ while running:
                                     and event.key == p.K_ESCAPE):
             running = False
 
+        if event.type == SPAWN_METEOR:
+            # Not the best approach. It'll be improved
+            # in the next lesson.
+            meteor_image = p.image.load(
+                'res/PNG/Meteors/'+rnd.choice(meteor_name_list))
+            meteor = sprites.Meteor(
+                (rnd.randint(0,SCREEN_WIDTH), -20), meteor_image)
+            meteor_group.add(meteor)
+
+    # Drawing
+    screen.blit(background_img, (0, 0))
     ship.draw(screen)
+    meteor_group.draw(screen)
+
+    # Updating
     ship.move()
+    meteor_group.update()
 
     clock.tick(60)
     p.display.flip()
--- sprites.py
+++ sprites.py
@@ -1,4 +1,6 @@
+import random as rnd
 import pygame as p
+from settings import *
 
 
 class Spaceship:
@@ -21,4 +23,18 @@ class Spaceship:
         if keys[p.K_s]:
             self.rect.y += 5
 
+
+class Meteor(p.sprite.Sprite):
+    def __init__(self, pos, image):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect(center=pos)
+        self.speed_x = rnd.randint(-3, 3)
+        self.speed_y = rnd.randint(3, 9)
+
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.y > SCREEN_HEIGHT:
+            self.kill()
 
